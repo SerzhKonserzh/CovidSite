d1468fd994c2c0a7a340cbe5532efc386dfdca76
SerzhKonserzh <Serzh-2001BTR@mail.ru>
2022-06-18 01:22:25 +0300

    feat: add header

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('.menu__icon');
+if(menuIcon){
+    const menuBody = document.querySelector('.menu__body');
+    menuIcon.addEventListener("click", function(e){
+        menuIcon.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
